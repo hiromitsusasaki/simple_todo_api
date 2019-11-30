127f60558921545a5cb014da7bb4859342062235
hiromitsu sasaki <hiromitsu.sasaki@gmail.com>
2019-11-30 18:56:27 +0900

    implement action methods

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::TasksController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  def index
+    tasks = Task.order(created_at: :desc)
+    render json: { status: 'SUCCESS', message: 'Loaded tasks', data: tasks }
+  end
+
+  def show
+    render json: { status: 'SUCCESS', message: 'Loaded the task', data: @tasks }
+  end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: { status: 'SUCCESS', data: task }
+    else
+      render json: { status: 'ERROR', data: task.errors }
+    end
+  end
+
+  def destroy
+    @task.destroy
+    render json: { status: 'SUCCESS', message: 'Deleted the task', data: @task }
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: { status: 'SUCCESS', message: 'Updated the post', data: @task }
+    else
+      render json: { status: 'SUCCESS', message: 'Not updated', data: @task.errors }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
